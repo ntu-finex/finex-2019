06749c79e782ffa1062e5bfb8b5beaa3abe5874e
Tan Jia Le <tanjiale1029@gmail.com>
2019-01-06 17:41:45 +0800

    minor fix

--- secure/connect.php
+++ secure/connect.php
@@ -1,13 +1,13 @@
 <?php
 
-	$connection = mysqli_connect("localhost","root","","ntu-iic_database") or
-	die ("Having trouble connect to database.".mysqli_error($connection));
+	// $connection = mysqli_connect("localhost","root","","ntu-iic_database") or
+	// die ("Having trouble connect to database.".mysqli_error($connection));
 
-	// if(mysqli_connect("localhost","ntu-i_root", "a1a2a3", "ntu-iic_database")){
-	// 	$connection = mysqli_connect("localhost","root", "", "ntu-iic_database");
-	// 	echo "Connected to database successfully";
-	// }else{
-	// 	die("Connection to database failed".mysqli_error($connection));
-	// }
+	if(mysqli_connect("localhost","ntu-i_root", "a1a2a3", "ntu-iic_database")){
+		$connection = mysqli_connect("localhost","root", "", "ntu-iic_database");
+		echo "Connected to database successfully";
+	}else{
+		die("Connection to database failed".mysqli_error($connection));
+	}
 	
 ?>
