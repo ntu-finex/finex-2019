b6b343676239fd2315e31232539e36d5b8031b2c
Tan Jia Le <tanjiale1029@gmail.com>
2019-01-06 17:43:08 +0800

    minor bug fix 3

--- secure/connect.php
+++ secure/connect.php
@@ -4,7 +4,7 @@
 	// die ("Having trouble connect to database.".mysqli_error($connection));
 
 	if(mysqli_connect("localhost","ntu-i_root", "a1a2a3", "ntu-iic_database")){
-		$connection = mysqli_connect("localhost","root", "", "ntu-iic_database");
+		$connection = mysqli_connect("localhost","ntu-i_root", "a1a2a3", "ntu-iic_database");
 	}else{
 		die("Connection to database failed".mysqli_error($connection));
 	}
