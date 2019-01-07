accc77bb5fa2e50df356be8f3346c31791715cc3
Tan Jia Le <tanjiale1029@gmail.com>
2019-01-08 00:30:15 +0800

    changed again

--- secure/config.php
+++ secure/config.php
@@ -2,6 +2,6 @@
 
 define("DB_HOST", "localhost");
 define("DB_NAME", "ntu-iic_database");
-define("DB_USERNAME", "ntu-iic_root");
+define("DB_USERNAME", "ntu-i_root");
 define("DB_PASSWORD", "a1a2a3");
 ?>
